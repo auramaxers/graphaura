8b21f317ddb8b7f0666fd6ec44d8d8047871ded2
Mike <mishalolhopengok@gmail.com>
2025-09-27 23:39:26 -0400

    Revert "liamellison02/graphaura"
    This reverts commit a9993c5c8377d942d5d5377c955f02565e4a34af.

--- backend/src/__init__.py
+++
@@ -1,3 +0,0 @@
-"""GraphAura Backend - Multi-modal knowledge graph system."""
-
-__version__ = "0.1.0"
--- backend/src/database/__init__.py
+++
@@ -1,7 +0,0 @@
-from .postgres import PostgresDB
-from .neo4j import Neo4jDB
-
-__all__ = [
-    "PostgresDB",
-    "Neo4jDB",
-]
